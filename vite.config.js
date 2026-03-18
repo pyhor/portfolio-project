ee7a1daf01308e9ec56601eef0caa7f994dbfba2
pyhor <165729654+pyhor@users.noreply.github.com>
2026-03-18 15:58:48 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? '/portfolio-project/' : '/',
-  }
-})
+// https://vite.dev/config/
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+
+  // 👇 IMPORTANT for GitHub Pages
+  base: command === 'build' ? '/portfolio-project/' : '/',
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+}))
