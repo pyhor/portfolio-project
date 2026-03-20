ab78cd35ded58ff84dddc5253a4c341c8efb9a04
pyhor <165729654+pyhor@users.noreply.github.com>
2026-03-20 18:41:33 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
+  base: '/portfolio-project/',  // MUST match your repo name
   plugins: [react()],
-
-  base: command === 'build' ? '/portfolio-project/' : '/',
-
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   },
-}))
+});
