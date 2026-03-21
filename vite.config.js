d783dba59db8838138a3250fdd9b51920b717139
pyhor <165729654+pyhor@users.noreply.github.com>
2026-03-21 11:19:46 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,11 @@ export default defineConfig({
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+      },
+    },
   },
 });
